1381011625252ab95e77afda04f3065782d94318
Brindasiva <124075213+Brindasiva@users.noreply.github.com>
2023-02-01 04:22:36 -0500

    added implementations for new classes.

--- src/main/java/brinda/inheritancesampleprograms/android.java
+++ src/main/java/brinda/inheritancesampleprograms/android.java
@@ -1,5 +1,38 @@
 package brinda.inheritancesampleprograms;
 
 public class android {
+	String colour;
+	int cost;
+	String RAM;
+	
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	public int getCost() {
+		return cost;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	public String getRAM() {
+		return RAM;
+	}
+	public void setRAM(String rAM) {
+		RAM = rAM;
+	}
+
+
+	String android_version;
+
+	public String getAndroid_version() {
+		return android_version;
+	}
+
+	public void setAndroid_version(String android_version) {
+		this.android_version = android_version;
+	}
 
 }
--- src/main/java/brinda/inheritancesampleprograms/samsung.java
+++ src/main/java/brinda/inheritancesampleprograms/samsung.java
@@ -1,5 +1,19 @@
 package brinda.inheritancesampleprograms;
 
-public class samsung {
+public class samsung extends android {
 
+	int serial_num;
+	String model;
+	public int getSerial_num() {
+		return serial_num;
+	}
+	public void setSerial_num(int serial_num) {
+		this.serial_num = serial_num;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
 }
